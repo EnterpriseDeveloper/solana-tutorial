f86799fba492b4d39e2bfe625f73e0c63f630d6f
EnterpriseDeveloper <voroshilovmax90@gmail.com>
2022-01-26 13:02:22 +0200

    finish with smart contract

--- myepicproject/tests/myepicproject.js
+++ myepicproject/tests/myepicproject.js
@@ -1,14 +1,51 @@
-const anchor = require('@project-serum/anchor');
+const anchor = require("@project-serum/anchor");
 
-const main = async() => {
-  console.log("🚀 Starting test...")
+// Need the system program, will talk about this soon.
+const { SystemProgram } = anchor.web3;
 
-  anchor.setProvider(anchor.Provider.env());
+const main = async () => {
+  console.log("🚀 Starting test...");
+
+  // Create and set the provider. We set it before but we needed to update it, so that it can communicate with our frontend!
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
   const program = anchor.workspace.Myepicproject;
-  const tx = await program.rpc.startStuffOff();
+
+  // Create an account keypair for our program to use.
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  // Call start_stuff_off, pass it the params it needs!
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
 
   console.log("📝 Your transaction signature", tx);
-}
+
+  // Fetch data from the account.
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("👀 GIF Count", account.totalGifs.toString());
+
+  // You'll need to now pass a GIF link to the function! You'll also need to pass in the user submitting the GIF!
+  await program.rpc.addGif("https://media.giphy.com/media/144Q1gg0FkTEVG/giphy.gif", {
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+    },
+  });
+
+  // Call the account.
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("👀 GIF Count", account.totalGifs.toString());
+
+  // Access gif_list on the account!
+  console.log("👀 GIF List", account.gifList);
+};
 
 const runMain = async () => {
   try {
